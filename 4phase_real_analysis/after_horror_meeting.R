039f55685454e8de290c49fcb886a427e20569bb
cieplins <73887754+cieplins@users.noreply.github.com>
2023-10-13 12:37:21 +0200

    aic calculation, removing bootstrap from this file

--- 4phase_real_analysis/after_horror_meeting.R
+++ 4phase_real_analysis/after_horror_meeting.R
@@ -4,16 +4,21 @@ library(ordinal)
 library(optimx)
 library(stats)
 library(data.table)
+library(boot)
 
 #Here choose the csv, it can be with real p1204 or desired only
-all_data <- read_csv("phase4_current_results_p1204.csv", col_types = "ffnnnnnnnnnnncc")
+all_data <- read_csv("phase4_current_results_p1204.csv", col_types = "fnnnnnnnnnnnncc")
 #Here choose for which data you want the model: 
 #n<8 for all data, n==7 for only full weeks and n<7 for only unfull weeks
-data_by_weeks_n <- all_data  %>% filter(n == 7)
+data_by_weeks_n <- all_data  %>% filter(n < 8)
 #Here choose what content you want: 
 #n < 26 for all content, n < 14 for nature only and n > 13 for slowmo only
-data_by_weeks <- data_by_weeks_n %>% filter(n < 26)
+data_by_weeks <- data_by_weeks_n %>% filter(week < 26)
 #if you want to exclude some testers you can do this after this line
+
+# Clean data (tester excluding)  ============================
+
+#data_by_weeks <- data_by_weeks[!(data_by_weeks$external_id %in% c("f1308", "bb8d0", "343ab", "9d7d1", "3fe63", "57ea9")), ]
 
 # Different classes of weighting functions ============================
 kumaraswamy_weights <- function(steps, a, b){
@@ -45,8 +50,20 @@ f_mean <- function(x) {
   return(tmp / norm)
 }
 
-data_by_weeks$f_mean = f_mean(as.matrix(data_by_weeks[,c("mo", "tu", "we", "th", "fr", "sa")]))
+f_min <- function(x) {
+  tmp <- x[,1]
+  tmp[is.na(x[,1])] <- Inf  # Initialize with positive infinity
+  for (i in c(2:6)){
+    tmp_na <- x[,i]
+    tmp_na[is.na(tmp_na)] <- Inf  # Treat missing values as positive infinity
+    tmp <- pmin(tmp, tmp_na)  # Calculate the element-wise minimum
+  }
+  tmp[tmp == Inf] <- NA  # Replace positive infinity with NA
+  return(tmp)
+}
 
+data_by_weeks$f_mean = f_mean(as.matrix(data_by_weeks[,c("mo", "tu", "we", "th", "fr", "sa")]))
+data_by_weeks$f_min = f_min(as.matrix(data_by_weeks[,c("mo", "tu", "we", "th", "fr", "sa")]))
 data_by_weeks$os <- factor(data_by_weeks$q2, ordered = TRUE, 
                            levels = c(1, 2, 3, 4, 5))
 
@@ -98,93 +115,38 @@ summary(opt_model)
 ggplot(NULL, aes(steps[2:7], kumaraswamy_weights(steps, opt_param$a, opt_param$b))) + 
   geom_point()
 
-# 1. Predict probabilities
-new_data <- data.frame(f_best_fun = data_by_weeks$f_best_fun)
-predicted_probs <- predict(opt_model, newdata = new_data, type = "prob")
 
-# Convert predicted_probs from list to matrix
-predicted_probs_matrix <- do.call(rbind, predicted_probs)
-
-# Pre-allocate a matrix to store the samples
-N <- 100
-random_samples_matrix <- matrix(0, nrow = nrow(predicted_probs_matrix), ncol = N)
-
-# Draw random levels based on predicted probabilities for each observation
-for(i in 1:nrow(predicted_probs_matrix)) {
-  random_samples_matrix[i, ] <- sample(1:ncol(predicted_probs_matrix), 
-                                       size = N, 
-                                       replace = TRUE, 
-                                       prob = predicted_probs_matrix[i, ])
-}
-
-tmp_data <- data_by_weeks[,c("mo", "tu", "we", "th", "fr", "sa", "q2")]
-n_steps <- length(steps) - 1
-points_bootstrap <- matrix(0, nrow = n_steps*ncol(random_samples_matrix), ncol = 3)
-for(i in 1:ncol(random_samples_matrix)) {
-  tmp_data$q2 <- random_samples_matrix[ ,i]
-  tmp_data$os <- factor(tmp_data$q2, ordered = TRUE, levels = c(1, 2, 3, 4, 5))
-  init_params <- kum_par[3,] # uniform distribution
-  lower_bounds <- c(0.00001, 0.00001)
-  opt_result <- optimx(init_params, objective_fun, method="L-BFGS-B", 
-                       lower=lower_bounds, df = tmp_data, steps = steps)
-  # TODO protect against estimation error
-  if (opt_result$kkt1 == TRUE & opt_result$kkt2 == TRUE) {
-    print(kumaraswamy_weights(steps, opt_result$a, opt_result$b))
-    points_bootstrap[((i - 1)*n_steps + 1):(i*n_steps), 1] = i
-    points_bootstrap[((i - 1)*n_steps + 1):(i*n_steps), 2] = steps[2:(n_steps + 1)]
-    points_bootstrap[((i - 1)*n_steps + 1):(i*n_steps), 3] = kumaraswamy_weights(steps, opt_result$a, opt_result$b)
-  }
-  else {
-    points_bootstrap[((i - 1)*n_steps + 1):(i*n_steps), 1] = i
-    points_bootstrap[((i - 1)*n_steps + 1):(i*n_steps), 2] = steps[2:(n_steps + 1)]
-    points_bootstrap[((i - 1)*n_steps + 1):(i*n_steps), 3] = NA
-    
-  }
-
-}
-points_bootstrap <- na.omit(points_bootstrap)
-bootstrap_data <- tibble(rep = points_bootstrap[ ,1], step = points_bootstrap[ ,2], weight = points_bootstrap[ ,3])
-ggplot(bootstrap_data, aes(step, weight)) + geom_point(alpha = 0.1)
-
-bootstrap_data$get_in <- 1
-# this piece is wrong, we need to remove max and min for each step until we get less than 95% of all samples
-# czyli tutaj trzeba zrobić to samo ale tylko dla stepów o tej samej wartosci
-bootstrap_data %<>%
-  filter(get_in == 1) %>%
-  group_by(step) %>%
-  mutate(abs_dif = abs(weight - mean(weight))) %>%
-  ungroup()
-
-desired_length <- nrow(bootstrap_data) * 0.95
-setDT(bootstrap_data)
-
-while (nrow(bootstrap_data) > desired_length) {
-  # Filter by get_in, compute abs_dif and remove rows with max abs_dif rep
-  bootstrap_data <- bootstrap_data[get_in == 1]
-  bootstrap_data[, abs_dif := abs(weight - mean(weight)), by = step]
-  
-  reps_with_max_abs_dif <- bootstrap_data[abs_dif == max(abs_dif), unique(rep)]
-  bootstrap_data[rep %in% reps_with_max_abs_dif, get_in := 0]
-}
-
-# If you need the result as a data.frame
-bootstrap_data <- as.data.frame(bootstrap_data)
-ggplot(bootstrap_data, aes(step, weight, group = rep)) + geom_point(size = 1, alpha = 0.03)
-ggplot(bootstrap_data, aes(step, weight, group = rep)) + geom_line(size = 1, alpha = 0.03)
+AIC(opt_model)
+summary(opt_model)
 
 
-
-glz_data <- all_data %>%
-  filter(n == 7)
+glz_data <- data_by_weeks
 glz_data$os <- factor(glz_data$q2, ordered = TRUE,levels = c(1, 2, 3, 4, 5))
-model <- clm(os ~ mo + tu + we + th + fr + sa, data = glz_data, family = "binomial")  
-AIC(model)
-summary(model)
+model_glz1 <- clm(os ~ mo + tu + we + th + fr + sa, data = glz_data, family = "binomial")  
+AIC(model_glz1)
+summary(model_glz1)
 
 glz_data %<>%
   mutate(motu = (mo + tu)/2) %>%
   mutate(weth = (we + th)/2) %>%
   mutate(frsa = (fr + sa)/2)
-model <- clm(os ~ motu + weth + frsa, data = glz_data, family = "binomial")  
-AIC(model)
-summary(model)
+model_glz2 <- clm(os ~ motu + weth + frsa, data = glz_data, family = "binomial")  
+AIC(model_glz2)
+summary(model_glz2)
+
+model_mean <- clm(os ~ f_mean, data = data_by_weeks, family = "binomial")
+summary(model_mean)
+
+model_min <- clm(os ~ f_min, data = data_by_weeks, family = "binomial")
+summary(model_min)
+
+
+AIC(model_min)
+AIC(model_mean)
+AIC(model_glz1)
+AIC(model_glz2)
+AIC(opt_model) + 4
+
+
+
+  
