b640cfec2f085de1e74583d8731548e37ca7d4b3
Krzysztof Rusek <krusek@agh.edu.pl>
2023-10-13 14:10:40 +0200

    Initial commit of py project

---
+++ pyits/pyits/__main__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2023-present Krzysztof Rusek <krusek@agh.edu.pl>
+#
+# SPDX-License-Identifier: MIT
+
+import gsd
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+    pass
